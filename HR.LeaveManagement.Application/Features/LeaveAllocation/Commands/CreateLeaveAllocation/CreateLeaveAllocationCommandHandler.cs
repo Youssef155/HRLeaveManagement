656b6f67c6c1d6f12e4b1aab46a28d8f6ca98257
Youssef George <yousifkhaleel00@gmail.com>
2024-03-25 15:14:02 +0200

    Create Leave Allocation Command

--- HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandHandler.cs
@@ -1,7 +1,37 @@
-﻿namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation
-{
-    public class CreateLeaveAllocationCommandHandler
-    {
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
 
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation
+{
+    public class CreateLeaveAllocationCommandHandler : IRequestHandler<CreateLeaveAllocationCommand, Unit>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public CreateLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(CreateLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateLeaveAllocationCommandValidator(_leaveTypeRepository);
+            var validateResult = await validator.ValidateAsync(request);
+
+            if (validateResult.Errors.Any())
+                throw new BadRequestException("Invalid leave allocation request", validateResult);
+
+            var leavType = await _leaveTypeRepository.GetByIdAsync(request.LeaveTypeId);
+
+
+            var leaveAllocation = _mapper.Map<Domain.LeaveAllocation>(request);
+            await _leaveAllocationRepository.CreateAsync(leaveAllocation);
+
+            return Unit.Value;
+        }
     }
 }
--- HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandValidator.cs
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandValidator.cs
@@ -1,7 +1,25 @@
-﻿namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation
-{
-    public class CreateLeaveAllocationCommandValidator
-    {
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
 
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation
+{
+    public class CreateLeaveAllocationCommandValidator : AbstractValidator<CreateLeaveAllocationCommand>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveAllocationCommandValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+
+            RuleFor(q => q.LeaveTypeId).GreaterThan(0)
+                .MustAsync(LeaveTypeMustExist)
+                .WithMessage("{PropertyName} doesn't exist");
+        }
+
+        private async Task<bool> LeaveTypeMustExist(int id, CancellationToken token)
+        {
+            var leavetype = await _leaveTypeRepository.GetByIdAsync(id);
+            return leavetype != null;
+        }
     }
 }
